4aab70ab7193491f520b38b05c068ab6ebc0bfe3
William Nascimento <william08.dcc@gmail.com>
2023-08-02 16:43:55 -0300

    criando a classe

--- testes.py
+++ testes.py
@@ -6,6 +6,47 @@ def key_func(item):
     return len(item)
 
 
+def get_sequence_order(graph):
+    bordas = []
+    # PEGA AS EXTREMIDADES DO GRAFO
+    for node in graph.nodes:
+        grau_do_no = len(G.adj[node])
+        if grau_do_no == 1:
+            # É EXTREMIDADE DO GRAFO
+            bordas.append(node)
+    # print(bordas)
+
+    origem = bordas[0]
+    destinos = bordas[1:]
+
+    caminhos = nx.all_simple_paths(G=graph, source=origem, target=destinos)
+
+    # ORDENAR CAMINHOS POR TAMANHO
+    # ordenar as sequencias do menor para o maior caminho
+    path_matrix = []
+    for i, caminho in enumerate(caminhos):
+        lista = list(caminho)
+        # print(caminho)
+        # print(lista)
+        path_matrix.append(lista)
+    # print(path_matrix)
+
+    # ORDENA A MATRIZ POR TAMANHO
+    path_matrix.sort(key=key_func)
+
+    # print(path_matrix)
+
+    # MONTAR LISTA TOTAL
+    # adiciona a lista os nos na sequencia percorrendo os caminhos para cada nó novo.
+    sequence_order = []
+    for caminho in path_matrix:
+        for node in caminho:
+            if node not in sequence_order:
+                sequence_order.append(node)
+    print("ordem de sequencia")
+    return sequence_order
+
+
 G = nx.Graph()
 
 G.add_edge(1, 2)
@@ -21,47 +62,10 @@ G.add_edge(8, 4)
 G.add_edge(8, 9)
 G.add_edge(9, 10)
 
-bordas = []
-# PEGA AS EXTREMIDADES DO GRAFO
-for node in G.nodes:
-    grau_do_no = len(G.adj[node])
-    if grau_do_no == 1:
-        # É EXTREMIDADE DO GRAFO
-        bordas.append(node)
-# print(bordas)
-
-origem = bordas[0]
-destinos = bordas[1:]
-
-caminhos = nx.all_simple_paths(G=G, source=origem, target=destinos)
-
-# ORDENAR CAMINHOS POR TAMANHO
-# ordenar as sequencias do menor para o maior caminho
-path_matrix = []
-for i, caminho in enumerate(caminhos):
-    lista = list(caminho)
-    # print(caminho)
-    # print(lista)
-    path_matrix.append(lista)
-# print(path_matrix)
-
-# ORDENA A MATRIZ POR TAMANHO
-path_matrix.sort(key=key_func)
-
-# print(path_matrix)
-
-# MONTAR LISTA TOTAL
-# adiciona a lista os nos na sequencia percorrendo os caminhos para cada nó novo.
-sequence_order = []
-for caminho in path_matrix:
-    for node in caminho:
-        if node not in sequence_order:
-            sequence_order.append(node)
-print("ordem de sequencia")
-print(sequence_order)
+sequencia = get_sequence_order(G)
+print(sequencia)
 
 # IMPRIMIR O GRAFO
 plt.figure(1)
 nx.draw_networkx(G, pos=nx.spring_layout(G), with_labels=True)
 plt.show()
-
